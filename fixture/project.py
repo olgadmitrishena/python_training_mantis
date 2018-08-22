c12f994241d74e0af570415d428affee96b8a323
Olga Dmitrishena <olga.dmitrishena@gmail.com>
2018-08-22 00:12:48 -0700

    25.Созданы тесты для добавления и удаления проектов в Mantis

--- fixture/project.py
+++ fixture/project.py
@@ -2,65 +2,59 @@ from model.project import Project
 
 class ProjectHelper:
 
-
     def __init__(self, app):
         self.app = app
 
-    def open_projects_page(self):
-        wd = self.app.wd
-        if not wd.current_url.endswith("/manage_proj_page.php"):
-            wd.find_element_by_link_text("Manage").click()
-            wd.find_element_by_link_text("Manage Projects").click()
 
-    def create_new_project(self, project):
-        wd = self.app.wd
-        self.open_projects_page()
-        self.open_new_project_page()
-        self.fill_new_project_form(project)
-        wd.find_element_by_css_selector("input[value='Add Project']").click()
-        wd.find_element_by_link_text("Proceed").click()
-
-    def fill_new_project_form(self, project):
-        wd = self.app.wd
-        wd.find_element_by_name("name").click()
-        wd.find_element_by_name("name").clear()
-        wd.find_element_by_name("name").send_keys(project.name)
-        wd.find_element_by_xpath("//select[@name='status']/option[@value='%d']" % project.status).click()
-        wd.find_element_by_xpath("//select[@name='view_state']/option[@value='%d']" % project.view_state).click()
-        wd.find_element_by_name("description").click()
-        wd.find_element_by_name("description").clear()
-        wd.find_element_by_name("description").send_keys(project.description)
-
-
-    def open_new_project_page(self):
+    def open_project_page(self):
         wd = self.app.wd
         wd.find_element_by_xpath("//input[@value='Create New Project']").click()
 
+    def change_field_value_project(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
+    def fill_project_form(self, project):
+        wd = self.app.wd
+        self.change_field_value_project("name", project.name)
+        self.change_field_value_project("description", project.description)
+
+    def create_project(self, project):
+        wd = self.app.wd
+        self.return_home()
+        self.open_project_page()
+        # fill contact form
+        self.fill_project_form(project)
+        wd.find_element_by_css_selector("input[value='Add Project']").click()
+        wd.find_element_by_link_text("Proceed").click()
+        self.project_cache = None
+
+    def delete_project_by_id(self, project):
+        wd = self.app.wd
+        self.return_home()
+        wd.find_element_by_link_text("%s" % project.name).click()
+        # submit delete
+        wd.find_element_by_link_text("Delete").click()
+        self.return_home()
+        self.project_cache = None
+
+    def return_home(self):
+        wd = self.app.wd
+        if not wd.current_url.endswith("//manage_mantisbt-1.2.20/login_page.php"):
+            wd.find_element_by_link_text("Manage").click()
+            wd.find_element_by_link_text("Manage Projects").click()
+    project_cache = None
 
     def get_project_list(self):
-        wd = self.app.wd
-        self.open_projects_page()
-        project_cache = []
-        for row in wd.find_elements_by_css_selector("table.width100 tr.row-1"):
-            cells = row.find_elements_by_css_selector("td")
-            name = cells[0].text
-            project_cache.append(Project(name=name))
-        for row in wd.find_elements_by_css_selector("table.width100 tr.row-2"):
-            cells = row.find_elements_by_css_selector("td")
-            name = cells[0].text
-            project_cache.append(Project(name=name))
-        return list(project_cache)
-
-    def tmp_print(self, l):
-        print('\n')
-        lst = sorted(l, key=Project.id_or_max)
-        for li in lst:
-            print("project " + str(li))
-
-
-    def delete_random_project(self, project):
-        wd = self.app.wd
-        self.open_projects_page()
-        wd.find_element_by_link_text("%s" % project.name).click()
-        wd.find_element_by_css_selector("input[value='Delete Project']").click()
-        wd.find_element_by_css_selector("input[value='Delete Project']").click()
+        if self.project_cache is None:
+            wd = self.app.wd
+            self.return_home()
+            self.project_cache = []
+            for table in wd.find_elements_by_css_selector('tr'):
+                element = table.find_elements_by_css_selector('td')
+                text1 = element[0].text
+                self.project_cache.append(Project(name=text1))
+        return list(self.project_cache)
--- test/test_del_project.py
+++ test/test_del_project.py
@@ -1,14 +1,20 @@
+
 from model.project import Project
 import random
 
-def test_del_project(app):
+
+def test_del_projects(app):
     app.session.login("administrator", "root")
-    app.project.open_projects_page()
-    old_projects = app.project.get_projects_list()
+    assert app.session.is_logged_in_as("administrator")
+    app.project.return_home()
+    if len(app.project.get_project_list()) == 0:
+        app.project.create_project(Project(name="1", description="1"))
+    old_projects = app.project.get_project_list()
     project = random.choice(old_projects)
-    app.project.delete_random_project(project)
-    new_projects = app.project.get_projects_list()
+    app.project.delete_project_by_id(project)
+    new_projects = app.project.get_project_list()
     assert len(old_projects) - 1 == len(new_projects)
     old_projects.remove(project)
-    assert sorted(new_projects, key=lambda p: p.name) == sorted(old_projects, key=lambda p: p.name)
+    assert sorted(old_projects, key=lambda p: p.name) == sorted(new_projects, key=lambda p: p.name)
+
 
